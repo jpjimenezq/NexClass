285a686dba88db3e8da31146229824f01cf85b3a
AndresFe0 <afprietol2005@gmail.com>
2024-08-26 14:37:09 -0500

    CAmbios favoritos y tablas de DB

--- NextClassProject/settings.py
+++ NextClassProject/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'users/static'),
+    os.path.join(BASE_DIR, 'static'),
+]
