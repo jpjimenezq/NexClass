7f83b84c9bb9ae849716dc3df5bec9a78e527a91
jpjimenezq <jpjimenez0206@gmail.com>
2024-09-01 19:28:42 -0500

    RF05 completado, modificar credenciales

--- users/forms.py
+++ users/forms.py
@@ -29,3 +29,26 @@ class TeacherForm(forms.ModelForm):
             'availability',
             'mode'
         ]
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'profile_picture',
+            'address',
+            'password',
+        ]
+        widgets = {
+            'email': forms.EmailInput(),
+        }
+
+class EditTeacherForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+        fields = [
+            'specialties',
+            'biography',
+            'availability',
+            'mode'
+        ]
