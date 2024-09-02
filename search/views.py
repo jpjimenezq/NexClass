be0e784455dfcd6732040d83f6888f8da92a859b
jpjimenezq <jpjimenezq0206@gmail.com>
2024-09-01 23:53:26 -0500

    Desarrollo de RF02, Buscar por filtros

--- search/views.py
+++ search/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from users.models import Teacher
 
 # Create your views here.
+def teachers(request):
+    searchTeacher = request.GET.get('searchTeacher', '')
+    if searchTeacher:
+        teachers = Teacher.objects.filter(specialties__icontains=searchTeacher)
+    else:
+        teachers = []
+    return render(request, 'teachers.html', {'teachers': teachers, 'searchTeacher': searchTeacher})
