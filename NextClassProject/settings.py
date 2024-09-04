74fd101934590aa0b3560578b46ae8160f5b5e5e
jpjimenezq <jpjimenezq0206@gmail.com>
2024-09-03 23:09:40 -0500

    Creacion de app communication

--- NextClassProject/settings.py
+++ NextClassProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'search',
+    'communication',
 ]
 
 MIDDLEWARE = [
--- NextClassProject/urls.py
+++ NextClassProject/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('search.urls')),
+    path('', include('communication.urls')),
 ]
 
 if settings.DEBUG:
