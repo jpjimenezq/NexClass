08f080a5330103817dc2acccd1643efe2de5a4bd
jpjimenezq <jpjimenezq0206@gmail.com>
2024-09-03 23:07:02 -0500

    Desarollo en proceso de RF06, ver datos de profesores

--- search/urls.py
+++ search/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('teachers/', views.teachers, name='teachers'),
+    path('teachers/<int:teacher_id>', views.teachers_detail, name='teachers_detail'),
 ]
