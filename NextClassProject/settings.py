642527190f2adac0fa06a84b74c9c76671e9a409
jpjimenezq <jpjimenezq0206@gmail.com>
2024-09-11 03:14:00 -0500

    Algunos estilos agregados2

--- NextClassProject/settings.py
+++ NextClassProject/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -124,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "NextClassProject/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
