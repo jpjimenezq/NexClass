d55dfbce68cea112e1231f092ded7c0efa41bb2e
Andres-FelipeP <afprietol@eafit.edu.co>
2024-10-05 12:42:46 -0500

    estilos form rating

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import TeacherRating
 
+
 class TeacherRatingForm(forms.ModelForm):
     class Meta:
         model = TeacherRating
         fields = ['rating', 'comment']
         widgets = {
-            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5}),
-        }
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5, 'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),}
