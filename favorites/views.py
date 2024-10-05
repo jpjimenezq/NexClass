ac54139909290a3bdba68a8e0e058ad4560ae321
Andres-FelipeP <afprietol@eafit.edu.co>
2024-10-05 12:38:27 -0500

    visualizacionn de clases por profesor, mejora visualizacion de perfil profeso

--- favorites/views.py
+++ favorites/views.py
@@ -1,36 +1,34 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from users.models import Student, Teacher, Class, User, StudentFavoritesTeachers, StudentFavoritesClasses
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 
 
 # Create your views here.
 def add_favorite_class(request, class_id):
-    """Añade una clase a la lista de favoritos.html del estudiante."""
     student = get_object_or_404(Student, user=request.user)
     selected_class = get_object_or_404(Class, id=class_id)
 
-    # Crea o recupera el favorito
-    favorite, created = StudentFavoritesClasses.objects.get_or_create(
-        student=student,
-        student_class=selected_class
-    )
+    if StudentFavoritesClasses.objects.filter(student=student, student_class=selected_class).exists():
+        messages.warning(request, "Ya te anadiste la clase a favoritos.")
+        return redirect('class_detail', class_id=selected_class.id)
 
-    return redirect('student_classes')
+    StudentFavoritesClasses.objects.create(student=student, student_class=selected_class)
+    messages.success(request, "¡Anadiste correctamente a favoritos esta clase!")
+    return redirect('class_detail', class_id=selected_class.id)
 
 
 def add_favorite_teacher(request, teacher_id):
-    """Añade una clase a la lista de favoritos.html del estudiante."""
     student = get_object_or_404(Student, user=request.user)
     selected_teacher = get_object_or_404(Teacher, id=teacher_id)
+    if StudentFavoritesTeachers.objects.filter(student=student, teacher=selected_teacher).exists():
+        messages.warning(request, "Ya te anadiste el profesor a favoritos.")
+        return redirect('teachers_detail', teacher_id=selected_teacher.id)
 
-    # Crea o recupera el favorito
-    favorite, created = StudentFavoritesTeachers.objects.get_or_create(
-        student=student,
-        teacher=selected_teacher
-    )
+    StudentFavoritesTeachers.objects.create(student=student, teacher=selected_teacher)
+    messages.success(request, "¡Anadiste correctamente a favoritos este profesor!")
+    return redirect('teachers_detail', teacher_id=selected_teacher.id)
 
-    return redirect('teachers')
 
 @login_required
 def favorites_view(request):
