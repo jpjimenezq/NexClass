cdc9e5667f39e843c292a042399fbe54b31c7351
ssantamars <ssantamars@eafit.edu.co>
2024-10-14 06:11:02 -0500

    Delete Blog function

--- teacher_blog/urls.py
+++ teacher_blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_blog_post, name='create_blog_post'),
+    path('edit/<int:pk>/', views.edit_blog_post, name='edit_blog_post'),
+    path('delete/<int:pk>/', views.delete_blog_post, name='delete_blog_post'),
 ]
